616fda6d3b1f9dc41fc01e5a28f0da8f588388ab
Dr Herilala <hei.teddy.3@gmail.com>
2025-08-14 15:35:28 +0300

    feat: add expense in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
+import expenseRoutes from './routes/expense.route.js'
 
 dotenv.config();
 const app = express();
@@ -11,5 +12,6 @@ app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth', authRoutes);
+app.use('/api/expenses', expenseRoutes);
 
 export default app;
