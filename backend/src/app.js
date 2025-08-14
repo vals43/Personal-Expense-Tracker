376375eae44ea87439854e468175c2e8eff0e33c
Dr Herilala <hei.teddy.3@gmail.com>
2025-08-14 14:18:04 +0300

    add: JWT athentication in back

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js';
 
+dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+
 app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 
 export default app;
