b39308a66debe5ac23b1d4fd03eed3e93a63ed5f
Gael Raj <hei.gael.30@gmail.com>
2025-08-22 16:24:16 +0300

    Add categories routes to app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/user.routes.js';
+import categoriesRouter from './routes/categories.routes.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/users', userRoutes);
+app.use('/api/categories',categoriesRouter);
 
 export default app;
